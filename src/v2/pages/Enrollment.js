c4c77534a44b22777db8442ebe4de541f97cf466
orlhatundji <orlhatundji@gmail.com>
2021-09-25 17:08:13 +0100

    added form validation

--- src/v2/pages/Enrollment.js
+++ src/v2/pages/Enrollment.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import styled from 'styled-components';
 import Select from 'react-select'
 import { Link } from 'react-router-dom';
