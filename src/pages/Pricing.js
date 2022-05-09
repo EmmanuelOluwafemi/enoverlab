24630244156cef149e8f2cc4bcaca20b8c3fe4c1
tuasegun <tuase360@gmail.com>
2022-05-09 13:24:16 +0100

    new message

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -47,16 +47,16 @@ const Pricing = () => {
         <StyledPricingWrapper>
              <div className="adbox">
                 <div className="text-box-1">
-                    <h3 className='web-text'>Admission closing soon; <br/> Classes start on the <br/> 9th of May</h3>
-                    <h3 className='mobile-text'>Admission closing soon; Classes start on the <br/>  9th of May</h3>
+                    <h3 className='web-text'>Indepth product management <br/>  training for absolute beginners</h3>
+                    <h3 className='mobile-text'>Indepth product management training<br/>for absolute beginners</h3>
                 </div>
                 <div className="text-box-2">
                     <img className="triangleWeb" src={triangle} alt="" />
                     <img src={triangleMobile} className="triangleMobile" alt="" />
                 </div>
                 <div className="text-box-3">
-                    <p className='web-text'>Installment payment option available<br/> <span>Enroll below</span></p>
-                    <p className='mobile-text'>Installment payment option available <br/> <span>Enroll below</span></p>
+                    <p className='web-text'>Classes starts this week<br/> Installment payment available <br/>  <span>Enroll below</span></p>
+                    <p className='mobile-text'>Classes starts this week <br/>  Installment payment available<br/><span>Enroll below</span></p>
                 </div>
             </div>
             <Header />
