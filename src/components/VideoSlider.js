9a7fa55c680214faedf0da8547405115ae9c3d35
tuasegun <tuase360@gmail.com>
2022-05-22 16:58:33 +0100

    fix title

--- src/components/VideoSlider.js
+++ src/components/VideoSlider.js
@@ -78,7 +78,7 @@ const VideoSlider = (props) => {
     <div className="textContainer">
         <h1  data-aos="fade-top" 
         data-aos-easing="ease-in-sine"
-        data-aos-duration="20000">Hear what our students have to say</h1>
+        data-aos-duration="20000">See the products our trained students have worked on</h1>
     </div>
 
 
