485ce766a113a714314cedbf0bad053d7a4a1108
tuasegun <tuase360@gmail.com>
2022-08-03 06:24:29 +0100

    new event

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 // images
 import banner from '../assets/img/bannerBackground.jpg';
 import ama from '../assets/img/ama.svg';
-import character from '../assets/img/banner_character.jpeg';
+import character from '../assets/img/bannerCharacter.jpg';
 
 
 const Banner = () => {
@@ -16,8 +16,8 @@ const Banner = () => {
                 <div className='cont'>
                     <img src={character} className="characterMobile" alt="character" />
                     <div className="details mobile">
-                     <h4>Kehinde Olukoga </h4>
-                     <p>Product Manager, Primera MFBank</p>
+                     <h4> Yewande Oyebo  </h4>
+                     <p>Product Owner, JustEatTakeaway.com</p>
                         {/* <p> Max</p> */}
 
                     </div>
@@ -25,15 +25,15 @@ const Banner = () => {
             </div>
             <div className="content">
                 <h5>This week product session topic:</h5>
-                <h2>The basics of building a Fintech product</h2>
+                <h2>How to get an International Product Management job</h2>
 
                 {/* <p>(Blackberry, Nokia, Sony, OLX, Lagos Red Taxi, Mr Biggs)</p> */}
             </div>
             <div className="cont">
                 <img src={character} className="characterWeb" alt="character" />
                 <div className="details web">
-                <h4>Kehinde Olukoga </h4>
-                <p>Product Manager, Primera MFBank</p>
+                <h4> Yewande Oyebo  </h4>
+                <p>Product Owner, JustEatTakeaway.com</p>
                 {/* <p> Max</p> */}
 
                 </div>
