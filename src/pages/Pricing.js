61cb1d9cc05c8403bf792a101f6d683bc8c2a14a
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2022-10-21 09:52:01 +0100

    fixed isseus

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -78,8 +78,8 @@ const Pricing = () => {
                     <img src={triangleMobile} className="triangleMobile" alt="" />
                 </div>
                 <div className="text-box-3">
-                    <p className='web-text'>Admission closes on November 7, 2022 <br/>   <span>Enroll below</span></p>
-                    <p className='mobile-text'>Admission closes on November 7, 2022 <br/><span>Enroll below</span></p>
+                    <p className='web-text'> Training starts November 10, 2022 <br/>   <span>Enroll below</span></p>
+                    <p className='mobile-text'> Training starts November 10, 2022<br/><span>Enroll below</span></p>
                 </div>
             </div>
             <Header />
