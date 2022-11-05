0d2d3065490155bb8d8017f7ea33415b7146c859
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2022-11-05 05:41:59 +0100

    fixed issues

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -80,10 +80,8 @@ const Pricing = () => {
                     <img src={triangleMobile} className="triangleMobile" alt="" />
                 </div>
                 <div className="text-box-3">
-                    <p className='web-text'>20% discount for the first 10 persons to apply<br/>
-                   <br/>   <span>Enroll below</span></p>
-                    <p className='mobile-text'> 20% discount for the first 10 persons to apply<br/>
-                    <br/><br/><span>Enroll below</span></p>
+                    <p className='web-text'>Exclusive Training starts November 9 <br/> <span>Enroll below</span></p>
+                    <p className='mobile-text'> Exclusive Training starts November 9<br/><span>Enroll below</span></p>
                 </div>
             </div>
             <Header />
